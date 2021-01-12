460d79dc29e8a10182a4ad15df42fbd938012faa
Wetido <wetido@gmail.com>
2021-01-12 20:13:36 +0100

    Firebase

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 import Posts from './components/Posts.vue'
 
 export default {
+
   name: 'App',
   components: {
     Posts
--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,11 +1,38 @@
 <template>
   <div class="main-container">
-    
+      <div>
+        <div v-if="this.items === null">Ladowanie</div>
+        <div v-else="" v-for="item in items" v-bind:key="item.id">{{item.content}}</div>
+      </div>
   </div>
 </template>
 
 <script>
+import firebase from "../database/firebase.js";
+
 export default {
+  data () {
+      return {
+        items: null,
+        update: false,
+      }
+    },
+  created () {
+
+    this.fetchData()
+  },
+
+  methods: {
+      async fetchData() {
+        const db = firebase.firestore();
+        const date = await db.collection("posts")
+
+        date.onSnapshot((data) => {
+              this.items = (data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        })
+        this.update = true
+    }
+  },
   name: 'Posts',
   props: {
     msg: String
@@ -15,8 +42,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.main-container{
-  padding: 30px;
-  background-color: black;
-}
+  .main-container{
+    padding: 30px;
+  }
 </style>
