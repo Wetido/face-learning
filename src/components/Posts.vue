a8a29a5adc040975d302947bab0d8401fb390593
Wetido <wetido@gmail.com>
2021-01-12 19:02:38 +0100

    Clearing up project

---
+++ src/components/Posts.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-container">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Posts',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-container{
+  padding: 30px;
+  background-color: black;
+}
+</style>
