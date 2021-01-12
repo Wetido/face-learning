3e4fa11fc9fa11d5d58273866aba9edcaa7458af
Wetido <wetido@gmail.com>
2021-01-12 23:19:05 +0100

    Add icon in navbar

--- src/components/AddPostModal.vue
+++ src/components/AddPostModal.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-button @click="modalShow = !modalShow">Open Modal</b-button>
+        <div class="plus" @click="modalShow = !modalShow"></div>
 
         <b-modal v-model="modalShow" @ok="handleOk">
             <label>Tytuł</label><input type="text" v-model="header" placeholder="Tytuł">
@@ -68,3 +68,53 @@
         }
     }
 </script>
+
+<style scoped>
+
+.plus{
+    position: relative;
+    width: 30px;
+    height: 30px;
+    margin-right: 30px;
+}
+
+.plus::before, 
+.plus::after{
+    content: "";
+    position: absolute;
+    background-color: white;
+    transition: transform 0.25s ease-out;
+}
+
+
+.plus:before{
+    top: 0;
+    left: 50%;
+    width: 4px;
+    height: 100%;
+    margin-left: -2px;
+}
+
+.plus:after{
+    top: 50%;
+    left: 0;
+    width: 100%;
+    height: 4px;
+    margin-top: -2px;
+}
+    
+.plus:hover{
+    cursor: pointer;
+}
+
+.plus:hover::before{
+    transform: rotate(180deg); 
+}
+
+.plus:hover::after{
+    transform: rotate(360deg); 
+}
+
+
+
+</style>
