260c66f16d5e81d02a76413f1e120d564dc229fd
Wetido <wetido@gmail.com>
2021-01-17 14:20:27 +0100

    Style fixes. Open and close chat

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,18 +1,39 @@
 <template>
-    <div class="chat-wrapper">
-        <div class="chat-header">Witamy na czacie internetowym</div>
-
-        <div class="messages-wrapper">
-            <div class="chat-list" v-for="item in messages" v-bind:key="item.id">
-                <div v-if="item.user === 'you'" class="post-header-you"><span>{{item.message}}</span></div>
-                <div v-else class="post-header-other"><span>{{item.message}}</span></div>
+<div>
+    <div class="chat-wrapper" id="wrapper">
+            <div class="chat-header-wrapper">
+                <div class="chat-header">Witamy na czacie internetowym</div>
+                <div class="close-button" @click="closeChat()">X</div>
             </div>
-        </div>
-        <div class="send-form">
-            <b-input class="send-input" v-model="message"></b-input>
-            <b-button @click="putData" class="send-button">Wyslij</b-button>
+
+            <div class="messages-wrapper">
+                <div class="chat-list" v-for="item in messages" v-bind:key="item.id">
+                    <div v-if="item.user === 'you'" class="post-header-you">
+                        <div class="spacing"></div>
+                        <div class="content">{{item.message}}</div>
+                    </div>
+                    <div v-else class="post-header-other">
+                        
+                        <div class="content">{{item.message}}</div>
+                        <div class="spacing"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="send-form">
+                <b-input class="send-input" v-model="message"></b-input>
+                <b-button @click="putData" class="send-button">Wyslij</b-button>
+            </div>
+
+    </div>
+
+    <div class="chat-wrapper-hidden" id="wrapper-hidden">
+        <div class="chat-header-wrapper">
+            <div class="chat-header">Otwórz czat</div>
+            <div class="close-button" @click="showChat()">^</div>
         </div>
     </div>
+
+</div>
 </template>
 
 <script>
@@ -70,7 +91,28 @@ export default {
                 
 
                 console.log(this.messages)
+            },
+
+            closeChat(){
+
+                const chatWrapper = document.getElementById('wrapper');
+                chatWrapper.style.visibility  = "hidden"
+
+                const closeChatWrapper = document.getElementById('wrapper-hidden')
+                closeChatWrapper.style.visibility  = "visible"
+            },
+
+            showChat(){
+
+                
+                const closeChatWrapper = document.getElementById('wrapper-hidden')
+                closeChatWrapper.style.visibility  = "hidden"
+
+                const chatWrapper = document.getElementById('wrapper');
+                chatWrapper.style.visibility  = "visible"
+
             }
+
 
     }
 }
@@ -78,10 +120,40 @@ export default {
 
 <style scoped>
 
-.chat-header{
 
+
+.chat-header-wrapper{
+    display: flex;
+    flex-wrap: wrap;
+    padding: 5px;
     background-color: indianred;
     border-radius: 5px;
+}
+
+.chat-wrapper-hidden{
+
+    width: 400px;
+    background-color: rgb(235, 230, 230);
+    border: 1px rounded black;
+    border-radius: 5px;
+    position: fixed;
+    float: right;
+    z-index: 50;
+    bottom: 0;
+    right: 15px;
+}
+
+#wrapper-hidden{
+    visibility: hidden;
+}
+
+.close-button{
+    margin-left: auto;
+    margin-right: 5px;
+}
+
+.chat-list{
+    margin: 15px 15px 15px 0px;
 }
 
 .chat-wrapper{
@@ -116,10 +188,12 @@ export default {
 }
 
 .messages-wrapper{
-    height: 90%;
+    height: 85%;
     overflow-y: scroll;
     overflow-x: hidden;
     margin-left: 5px;
+    
+    font-size: 1rem;
 }
 
 .messages-wrapper::-webkit-scrollbar{
@@ -133,28 +207,51 @@ export default {
 
 .post-header-you{
 
-    margin: 5px;
-    padding: 5px;
-    text-align: right;
-    width: 100%;
-}
+    margin: 2px 5px 2px 5px;
+    padding: 2px 5px 2px 5px;
+    display: flex;
+    flex-flow: row wrap;
+    align-items: flex-start;
 
-.post-header-you > span {
-    background-color: rgb(27, 184, 27);
-    padding: 7px;
-    border-radius: 10px;
 }
 
 .post-header-other{
 
-    margin: 5px;
-    padding: 5px;
-    text-align: left;
+    order: -1;
+    display: flex;
+    flex-flow: row wrap;
+    align-items: flex-start;
+    margin: 2px 5px 2px 5px;
+    padding: 2px 5px 2px 5px;
 }
 
-.post-header-other > span {
+.post-header-you > div {
+
+    background-color: rgb(27, 184, 27);
+    padding: 7px;
+    border-radius: 10px;
+    text-align: right;
+}
+
+.post-header-other > div {
+
     background-color: rgb(110, 110, 224);
     padding: 7px;
     border-radius: 10px;
 }
+
+
+.spacing{
+    width: 20%;
+    margin: 5px;
+    visibility: hidden;
+}
+
+.content{
+    width: 77%;
+}
+
+
+
+
 </style>
